200cc69a4f1557505ef10191ffb9f583dd18d67d
ryunex15 <badarudin1511@gmail.com>
2025-02-04 12:57:30 +0700

    users

--- resources/views/layouts/jumbotron.blade.php
+++ resources/views/layouts/jumbotron.blade.php
@@ -21,33 +21,46 @@
 <div class="w-full flex justify-center gap-6 mt-6 text-white">
     <div class=" rounded-xl shadow-xl min-h-[13rem] max-h-[13rem] min-w-[13rem] max-w-[13rem] dark:bg-blue-900/80 flex justify-center items-center">
         <a class="flex flex-col" href="">
-            <svg xmlns="http://www.w3.org/2000/svg" class="ionicon w-24 items-center" viewBox="0 0 512 512">
-                <path d="M368 415.86V72a24.07 24.07 0 00-24-24H72a24.07 24.07 0 00-24 24v352a40.12 40.12 0 0040 40h328" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32" />
-                <path d="M416 464h0a48 48 0 01-48-48V128h72a24 24 0 0124 24v264a48 48 0 01-48 48z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32" />
-                <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M240 128h64M240 192h64M112 256h192M112 320h192M112 384h192" />
-                <path d="M176 208h-64a16 16 0 01-16-16v-64a16 16 0 0116-16h64a16 16 0 0116 16v64a16 16 0 01-16 16z" />
+            <svg xmlns="http://www.w3.org/2000/svg" class="ionicon w-24 items-center ml-4" viewBox="0 0 512 512">
+                <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M176 464h160M256 464V336M384 224c0-50.64-.08-134.63-.12-160a16 16 0 00-16-16l-223.79.26a16 16 0 00-16 15.95c0 30.58-.13 129.17-.13 159.79 0 64.28 83 112 128 112S384 288.28 384 224z" />
+                <path d="M128 96H48v16c0 55.22 33.55 112 80 112M384 96h80v16c0 55.22-33.55 112-80 112" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" />
             </svg>
-            <h1 class="text-3xl">Kejuaraan</h1>
+            <h1 class="text-3xl text-center">Kejuaraan</h1>
 
         </a>
 
     </div>
     <div class=" rounded-xl shadow-xl min-h-[13rem] max-h-[13rem] min-w-[13rem] max-w-[13rem] dark:bg-blue-900/80 flex justify-center items-center">
         <a class="flex flex-col" href="">
-
-            <h1 class="text-3xl">Berita</h1>
+            <svg xmlns="http://www.w3.org/2000/svg" class="ionicon w-24 items-center" viewBox="0 0 512 512">
+                <path d="M368 415.86V72a24.07 24.07 0 00-24-24H72a24.07 24.07 0 00-24 24v352a40.12 40.12 0 0040 40h328" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32" />
+                <path d="M416 464h0a48 48 0 01-48-48V128h72a24 24 0 0124 24v264a48 48 0 01-48 48z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32" />
+                <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M240 128h64M240 192h64M112 256h192M112 320h192M112 384h192" />
+                <path d="M176 208h-64a16 16 0 01-16-16v-64a16 16 0 0116-16h64a16 16 0 0116 16v64a16 16 0 01-16 16z" fill="white" />
+            </svg>
+            <h1 class="text-3xl text-center">Berita</h1>
 
         </a>
     </div>
     <div class=" rounded-xl shadow-xl min-h-[13rem] max-h-[13rem] min-w-[13rem] max-w-[13rem] dark:bg-blue-900/80 flex justify-center items-center">
         <a href="">
+            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="white">
+                <g data-name="agenda book" class="w-24 items-center" id="agenda_book">
+                    <path d="M22,5.5A1.5,1.5,0,0,0,20.5,4H19V3.5A1.5,1.5,0,0,0,17.5,2H3.5A1.5,1.5,0,0,0,2,3.5v17A1.5,1.5,0,0,0,3.5,22h14A1.5,1.5,0,0,0,19,20.5V20h1.5A1.5,1.5,0,0,0,22,18.5v-3a1.49,1.49,0,0,0-.39-1,1.49,1.49,0,0,0,.39-1v-3a1.49,1.49,0,0,0-.39-1,1.49,1.49,0,0,0,.39-1ZM20.5,5a.5.5,0,0,1,.5.5v3a.5.5,0,0,1-.5.5H19V5Zm.5,5.5v3a.5.5,0,0,1-.5.5H19V10h1.5A.5.5,0,0,1,21,10.5ZM15,3V7.29L13.85,6.15a.48.48,0,0,0-.7,0L12,7.29V3ZM18,20.5a.5.5,0,0,1-.5.5H3.5a.5.5,0,0,1-.5-.5V3.5A.5.5,0,0,1,3.5,3H11V8.5a.5.5,0,0,0,.31.46.48.48,0,0,0,.54-.11L13.5,7.21l1.65,1.64A.47.47,0,0,0,15.5,9a.43.43,0,0,0,.19,0A.5.5,0,0,0,16,8.5V3h1.5a.5.5,0,0,1,.5.5v17Zm3-2a.5.5,0,0,1-.5.5H19V15h1.5a.5.5,0,0,1,.5.5Z" />
+                    <path d="M15.5,11H5.5a.5.5,0,0,0-.5.5v4a.5.5,0,0,0,.5.5h10a.5.5,0,0,0,.5-.5v-4A.5.5,0,0,0,15.5,11ZM15,15H6V12h9Z" />
+                    <path d="M15.5,18H5.5a.5.5,0,0,0,0,1h10a.5.5,0,0,0,0-1Z" />
+                </g>
+            </svg>
             <h1 class="text-3xl">Agenda</h1>
 
         </a>
     </div>
     <div class=" rounded-xl shadow-xl min-h-[13rem] max-h-[13rem] min-w-[13rem] max-w-[13rem] dark:bg-blue-900/80 flex justify-center items-center">
         <a href="">
-            <h1 class="text-3xl">Ekstrakurikuler</h1>
+            <svg viewBox="0 0 512 512" class="w-24 items-center" xmlns="http://www.w3.org/2000/svg" fill="white">
+                <path d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zm-48 0l-.003-.282-26.064 22.741-62.679-58.5 16.454-84.355 34.303 3.072c-24.889-34.216-60.004-60.089-100.709-73.141l13.651 31.939L256 139l-74.953-41.525 13.651-31.939c-40.631 13.028-75.78 38.87-100.709 73.141l34.565-3.073 16.192 84.355-62.678 58.5-26.064-22.741-.003.282c0 43.015 13.497 83.952 38.472 117.991l7.704-33.897 85.138 10.447 36.301 77.826-29.902 17.786c40.202 13.122 84.29 13.148 124.572 0l-29.902-17.786 36.301-77.826 85.138-10.447 7.704 33.897C442.503 339.952 456 299.015 456 256zm-248.102 69.571l-29.894-91.312L256 177.732l77.996 56.527-29.622 91.312h-96.476z" />
+            </svg>
+            <h1 class="text-3xl text-center">Ekskul</h1>
 
         </a>
     </div>
