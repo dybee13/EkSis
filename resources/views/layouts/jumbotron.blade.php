79d00b505aee3a960139fd367122329d2a14674a
ryunex15 <badarudin1511@gmail.com>
2025-02-04 08:18:53 +0700

    ambatron siap beraksi

--- resources/views/layouts/jumbotron.blade.php
+++ resources/views/layouts/jumbotron.blade.php
@@ -1,13 +1,19 @@
-<section class="bg-white dark:bg-gray-900 mt-8 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')]">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative">
-        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Selamat datang di Eksis !</h1>
-        <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">Sistem Pencatatan Eskul Siswa Digital</p>
-        <a href="#" class="inline-flex justify-center items-center py-3 px-5 mr-4 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-            Daftar Ekstrakulikuler
-        </a>
-        <a href="#" class="inline-flex justify-center items-center py-3 px-5 ml-4 text-base font-medium text-center text-white rounded-lg bg-gray-700 hover:bg-gray-500 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-            Tentang Aplikasi
-        </a>
+<section class="bg-white dark:bg-gray-900 min-h-screen flex items-center justify-center relative">
+    <div class="py-8 px-4 mx-auto max-w-screen-lg text-center lg:py-16 relative z-10">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+            Selamat datang di Eksis!
+        </h1>
+        <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-32 dark:text-gray-200">
+            Sistem Pencatatan Eskul Siswa Digital
+        </p>
+        <div class="flex flex-col sm:flex-row justify-center gap-4">
+            <a href="#" class="inline-flex justify-center items-center py-3 px-6 text-base font-medium text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                Daftar Ekstrakurikuler
+            </a>
+            <a href="#" class="inline-flex justify-center items-center py-3 px-6 text-base font-medium text-white rounded-lg bg-gray-700 hover:bg-gray-500 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-900">
+                Tentang Aplikasi
+            </a>
+        </div>
     </div>
-    <div class="bg-gradient-to-b from-blue-50 to-transparent dark:from-blue-900 w-full h-full absolute top-0 left-0 z-0"></div>
+    <div class="bg-gradient-to-b from-blue-50 to-transparent dark:from-blue-900 absolute inset-0"></div>
 </section>
